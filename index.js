5ee58dcc386d6284a046e8b95b0e2e666b5a073c
Omolola Odunowo <omololaodunowo@gmail.com>
2022-06-17 12:05:34 +0100

    finalized js authentication for registration form inputs

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
-function formValidation() {
-    let x = document.forms["formValidate"]["fname"].value;
-  if (x == "") {
-    alert("Name must be filled out");
-    return false;
+const firstName=document.getElementById("fname");
+const lastName= document.getElementById("lname");
+const email=document.getElementById("email");
+const password=document.getElementById("password");
+const form=document.getElementById("form");
+const error1=document.getElementById("error1");
+const error2=document.getElementById("error2");
+const error3=document.getElementById("error3");
+const error4=document.getElementById('error4');
+
+
+form.addEventListener("submit", (e)=>{
+  if(firstName.value === "" || firstName.value == null){
+    error1.style.display = "block";
+    firstName.style.border = "1px solid hsl(0,100%,74%)";
   }
-}
+
+  if (lastName.value === "" || lastName.value == null){
+    error2.style.display = "block";
+    lastName.style.border = "1px solid hsl(0,100%,74%)";
+  }
+
+  if(email.value === "" || email.value == null){
+    error3.style.display="block";
+    email.style.border = "1px solid hsl(0,100%,74%)";
+  }
+
+   if(password.value === "" || password.value == null){
+    error4.style.display="block";
+    password.style.border = "1px solid hsl(0,100%,74%)";
+  }
+ e.preventDefault();
+})
+
